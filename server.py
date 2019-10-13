6e2b5acc64998ebf2e6c5f6ce6f7c7d48509a269
Pras Velagapudi <psigen@gmail.com>
2019-10-13 14:03:14 -0400

    Switched to using server and client one-shot requests.

--- server.py
+++ server.py
@@ -17,26 +17,19 @@ try:
     print("Listening on {}".format(port))
     while True:
         newSocket, address = sock.accept()
-        newSocket.settimeout(5.0)
+        newSocket.settimeout(2.0)
         newSocket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, True)
-        print("Connected from {}".format(address))
 
-        # loop serving the new client
-        while True:
-            try:
-                print("Got here A")
-                receivedData = newSocket.recv(2)
-                print("Got here B ({})".format(receivedData))
-                time.sleep(0.1)
-
-                for i in range(10):
-                    payload = bytearray(
-                        random.getrandbits(8) for _ in range(frame_size // 10))
-                    newSocket.send(payload)
-                print("Got here C")
-            except Exception as e:
-                newSocket.close()
-                print("Disconnected from {}: {}".format(address, e))
-                break
+        try:
+            print("Connected from {}".format(address))
+            for i in range(10):
+                payload = bytearray(
+                    random.getrandbits(8) for _ in range(frame_size // 10))
+                newSocket.send(payload)
+        except:
+            pass
+        finally:
+            newSocket.close()
+            print("Disconnected from {}".format(address))
 finally:
     sock.close()
--- wifi_display.cpp
+++ wifi_display.cpp
@@ -24,8 +24,7 @@ int status = WL_IDLE_STATUS;
 uint16_t *frameBuffer;
 size_t frameIdx;
 constexpr size_t FRAME_BUFFER_BYTES = ARCADA_TFT_WIDTH * ARCADA_TFT_HEIGHT * sizeof(frameBuffer[0]);
-
-char bufferResponse[] = "ok";
+constexpr size_t FRAME_TIMEOUT = 5000;
 
 WiFiClient client;
 
@@ -45,22 +44,6 @@ void printWifiStatus()
   Serial.print("RSSI:");
   Serial.print(rssi);
   Serial.println(" dBm");
-}
-
-void flush()
-{
-  static uint8_t bufferDrain[100];
-  while (client.available())
-    client.read(bufferDrain, min(100, client.available()));
-}
-
-/** Request a new frame from the server. */
-void requestFrame()
-{
-  delay(100); // Frame limit at 10 fps.
-  flush();
-  frameIdx = 0;
-  client.print(bufferResponse);
 }
 
 void setColor(int r, int g, int b)
@@ -101,73 +84,48 @@ void WifiDisplay::setup()
 
 void WifiDisplay::loop()
 {
-  setColor(255, 0, 0);
+  delay(100); // Frame limit at 10 fps.
 
-  // Check if we are still connected.
-  if (!client.connected())
-  {
-    Serial.print("Connecting to server: ");
-    Serial.print(serverIp);
-    Serial.print(":");
-    Serial.print(serverPort);
-    Serial.print(" ... ");
-    if (!client.connect(serverIp, serverPort))
-    {
-      Serial.println("failed.");
-      return;
-    }
-    Serial.println("done.");
-    requestFrame();
-  }
+  Serial.print("Connecting to server: ");
+  Serial.print(serverIp);
+  Serial.print(":");
+  Serial.println(serverPort);
 
-  setColor(0, 0, 0);
-
-  // Manually send a frame request.
-  if (arcada.readButtons() & ARCADA_BUTTONMASK_B)
-  {
-    setColor(0, 255, 0);
-    while (arcada.readButtons() & ARCADA_BUTTONMASK_B)
-      delay(100);
-    Serial.println("Request (manual).");
-    requestFrame();
+  if (!client.connect(serverIp, serverPort))
     return;
-  }
+  setColor(255, 255, 255);
+  bzero(frameBuffer, FRAME_BUFFER_BYTES);
 
   // Read the packet into buffer.
-  int numBytes = 0;
-  do
+  long start = millis();
+  while (client.available() || client.connected())
   {
+    setColor(255, 0, 0);
+    size_t numBytes = client.read(reinterpret_cast<uint8_t *>(frameBuffer) + frameIdx, max(FRAME_BUFFER_BYTES - frameIdx, 0));
     setColor(0, 0, 255);
-    numBytes = client.read(reinterpret_cast<uint8_t *>(frameBuffer) + frameIdx, FRAME_BUFFER_BYTES - frameIdx);
-    setColor(255, 128, 0);
-    if (numBytes > 0)
-      frameIdx += numBytes;
-  } while (numBytes > 0);
+    if (numBytes < 0)
+      break;
+    frameIdx += numBytes;
+    if (frameIdx >= FRAME_BUFFER_BYTES)
+      break;
+    if (millis() - start > FRAME_TIMEOUT)
+      break;
+  };
 
-  setColor(255, 0, 255);
+  setColor(255, 128, 0);
+  client.stop();
+  setColor(128, 255, 0);
 
-  /**
   // Print number of bytes.
   Serial.print("Got ");
   Serial.print(frameIdx);
-  Serial.print(" (+");
-  Serial.print(numBytes);
-  Serial.println(") bytes.");
-  */
+  Serial.println(" bytes.");
 
-  // Ignore a partial buffer.
-  if (frameIdx < FRAME_BUFFER_BYTES)
-    return;
-  Serial.println("Frame.");
-
-  setColor(255, 255, 0);
+  setColor(0, 255, 0);
 
   // Blit this content to the display buffer.
   arcada.blitFrameBuffer(0, 0, true, false);
   frameIdx = 0;
 
-  // Request more data.
-  requestFrame();
-
-  setColor(255, 255, 255);
+  setColor(0, 0, 0);
 }
