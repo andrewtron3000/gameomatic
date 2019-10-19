0c5e9b59dddaa55e10c251c7852c14811549019b
Andrew <andrew.unit@gmail.com>
2019-10-19 10:13:13 -0700

    Modify to keep socket open between frames.

--- server.py
+++ server.py
@@ -3,14 +3,21 @@ import random
 import socket
 import time
 
-frame_size = 160 * 128 * 2
+frame_bytes = 160 * 128 * 2
 port = 2390
+payload = bytearray(frame_bytes)
+frame_count = 0
 
 # Create a socket.
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.bind(('', port))
 sock.listen(10)
+
+def sleep_print(secs):
+   for i in range(secs):
+      print(secs - i)
+      time.sleep(1)
 
 # Loop waiting for connections (terminate with Ctrl-C)
 try:
@@ -19,17 +26,18 @@ try:
         newSocket, address = sock.accept()
         newSocket.settimeout(2.0)
         newSocket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, True)
-
-        try:
-            print("Connected from {}".format(address))
-            for i in range(10):
-                payload = bytearray(
-                    random.getrandbits(8) for _ in range(frame_size // 10))
-                newSocket.send(payload)
-        except:
-            pass
-        finally:
-            newSocket.close()
-            print("Disconnected from {}".format(address))
+        print("Connected from {}".format(address))
+        while True:
+            try:
+                  for i in range(0, frame_bytes, 4):
+                     payload[i] = 0xff
+                     payload[i+1] = frame_count & 0xff
+                     payload[i+2] = (i >> 8) & 0xff
+                     payload[i+3] = i & 0xff
+                  newSocket.send(payload)
+                  frame_count += 1
+                  sleep_print(30)
+            except:
+                  pass
 finally:
     sock.close()
